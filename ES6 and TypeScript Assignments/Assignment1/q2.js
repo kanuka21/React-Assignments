0e653481db754d42497129d605ecbd9e282c4feb
kanuka goel <65423384+kanuka21@users.noreply.github.com>
2021-12-08 01:31:39 +0530

    Create q2.js

---
+++ ES6 and TypeScript Assignments/Assignment1/q2.js
@@ -0,0 +1,10 @@
+
+function myHalfName(name){
+    if(name=="Priyanka"|| name=="Priya") {
+    let message = "Hii, Priya!!!";
+    console.log(message); // Output: Hii, Priya!!!
+     }
+ console.log(message); // Output: Uncaught ReferenceError: message is not defined
+ }
+
+ myHalfName("Kanu");
