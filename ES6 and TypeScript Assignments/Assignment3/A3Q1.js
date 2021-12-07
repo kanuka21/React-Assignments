3571e89b31e8d899241ab7707fdaea488e6260e6
kanuka goel <65423384+kanuka21@users.noreply.github.com>
2021-12-08 02:05:23 +0530

    Add files via upload

---
+++ ES6 and TypeScript Assignments/Assignment3/A3Q1.js
@@ -0,0 +1,21 @@
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('The first promise has resolved');
+        resolve(10);
+    }, 1 * 1000);
+
+});
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('The second promise has resolved');
+        resolve(20);
+    }, 2 * 1000);
+});
+
+Promise.all([p1, p2])
+    .then(results => {
+        const total = results.reduce((p, c) => p + c);
+
+        console.log(`Results: ${results}`);
+        console.log(`Total: ${total}`);
+    });
